5f5343c3ef999951d0b65694f259b942abd9f760
Mathieu Piton <goore.csmoviz@gmail.com>
2024-12-21 13:24:53 +0100

    Update ecosystem configuration to include development environment settings

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,6 +5,9 @@ module.exports = {
       args: "run src/index.ts",
       watch: false,
       env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
         NODE_ENV: "production"
       }
     }]
